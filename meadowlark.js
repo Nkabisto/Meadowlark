a4501e88a50f3b140c7a3b625e1680e34417ffb3
Lionel <nkabisto@gmail.com>
2025-03-12 20:20:19 +0200

    Basics of Express

--- meadowlark.js
+++ meadowlark.js
@@ -1,21 +1,48 @@
-const express = require('express')
+import express from 'express'
+import { engine } from 'express-handlebars'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 const app = express()
+
+const fortunes = ["Conquer your fears or they will conquer you.",
+  "Rivers need springs.",
+  "Do not fear what you don't know.",
+  "You will have a pleasant surprise.",
+  "Whenever possible, keep it simple.",
+]
+app.use(express.static(__dirname + '/public'))
+
+// configure Handlebars view engine
+app.engine('handlebars', engine({
+  defaultLayout: 'main',
+}))
+app.set('view engine', 'handlebars')
 
 const port = process.envPORT || 3000
 
+app.get('/', (req,res) => res.render('home'))
+
+app.get('/about', (req, res) =>{
+  const randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)]
+  res.render('about', { fortune: randomFortune })
+})
+
+
 // custom 404 page
 app.use((req, res) => {
-  res.type('text/plain')
   res.status(404)
-  res.send('404 - Not Found')
+  res.render('404')
 })
 
 // custom 500 page
 app.use((err,req,res,next) => {
   console.error(err.message)
-  res.type('text/plain')
   res.status(500)
-  res.send('500 - Server Error')
+  res.render('500')
 })
 
 app.listen(port, () => console.log(
